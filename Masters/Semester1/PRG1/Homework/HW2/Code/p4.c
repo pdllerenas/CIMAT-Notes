76c977d21956fed34d5c827fc3a337030906c502
pdllerenas <pdllerenas@gmail.com>
2025-09-01 10:27:59 -0600

    template PRG1 HW2 p4

---
+++ Masters/Semester1/PRG1/Homework/HW2/Code/p4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char **create_array(char **arr, ...) {
+  char **a;
+  return a;
+}
+
+char **ordena(char **arr, ...) {
+  char **a;
+  return a;
+}
+
+int main() {
+  return 0;
+}
