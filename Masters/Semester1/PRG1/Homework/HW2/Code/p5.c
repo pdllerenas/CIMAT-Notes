4ddf8d496c1cbdbbf6ffa6f3e5c7d7d724951285
pdllerenas <pdllerenas@gmail.com>
2025-09-04 22:27:08 -0600

    finished p5 PRG1 HW2 code

--- Masters/Semester1/PRG1/Homework/HW2/Code/p5.c
+++ Masters/Semester1/PRG1/Homework/HW2/Code/p5.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int repetitions(char *str, char ch) {
   int ch_count = 0;
   while (*str) {
-    if (*str == ch) ch_count++;
+    if (*str == ch)
+      ch_count++;
+    str++;
   }
   return ch_count;
 }
 
 char **tokenize(char *str, char ch) {
   int ch_rep = repetitions(str, ch);
-  char **tokens = malloc(ch_rep*sizeof(*tokens));
-  while (*str) {
-    printf("%c\n", *str);
-    **tokens = *str;
-
-    if (*str == ch) {
-      tokens++;
+  char **token_list = malloc((ch_rep + 1) * sizeof(*token_list));
+  char temp_token[100];
+  int token_len = 0, token_count = 0;
+  do {
+    if (*str == ch || *str == '\0') {
+      char *token = malloc(token_len * sizeof(char) + 1);
+      temp_token[token_len] = '\0';
+      strcpy(token, temp_token);
+      *(token_list + token_count) = token;
+      token_count++;
+      token_len = 0;
+    } else {
+      temp_token[token_len] = *str;
+      token_len++;
     }
     str++;
-  }
-  return tokens;
+  } while (token_count <= ch_rep);
+  return token_list;
 }
 
 int main() {
-  char** tokenized = tokenize("Hello one two three four five", ' ');
-  for (int i = 0; i < 6; i++) {
-    printf("%s\n", *(tokenized+i));
+  char *str = "Hello one two three four five dd";
+  char tkn = ' ';
+  char **tokenized = tokenize(str, tkn);
+  for (int i = 0; i < 7; i++) {
+    printf("%s\n", *(tokenized + i));
   }
+  free(tokenized);
 }
