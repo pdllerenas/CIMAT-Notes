758f2adfdb31ad964c2e9591978eb13ecc7c52db
pdllerenas <pdllerenas@gmail.com>
2025-09-10 10:02:34 -0600

    added upper triangular matrix reader
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/NUM1/Homework/HW3/Code/p1.c
+++ Masters/Semester1/NUM1/Homework/HW3/Code/p1.c
@@ -68,6 +68,44 @@ double **create_lower_from_file(FILE *file, int *cols, int *rows) {
   return matrix;
 }
 
+double **create_upper_from_file(FILE *file, int *cols, int *rows) {
+  *rows = *cols = 0;
+  int c, row_cnt = 1;
+  get_matrix_dims(file, cols, rows);
+  char line[1024];
+  double **matrix = malloc(*rows * sizeof(double *));
+  if (matrix == NULL) {
+    fprintf(stderr, "Could not allocate memory for matrix.\n");
+    fclose(file);
+    exit(1);
+  }
+  int row_index, col_index;
+  while (fgets(line, sizeof line, file) && row_index < *rows) {
+    matrix[row_index] = calloc(*cols, sizeof(double));
+    if (matrix[row_index] == NULL) {
+      fprintf(stderr, "Could not allocate memory for matrix row %d.\n",
+              row_index);
+      for (int i = 0; i < *rows - row_index; i++) {
+        free(matrix[i]);
+      }
+      free(matrix);
+      fclose(file);
+      exit(1);
+    }
+    col_index = 0;
+    char *token = strtok(line, " ");
+    while (token != NULL && col_index < *rows - row_cnt) {
+      matrix[row_index][col_index] = atof(token);
+      token = strtok(NULL, " ");
+      col_index++;
+    }
+    row_index++;
+    row_cnt++;
+  }
+
+  fclose(file);
+  return matrix;
+}
 double **create_array_from_file(FILE *file, int *cols, int *rows) {
   *rows = *cols = 0;
   int row_cnt;
