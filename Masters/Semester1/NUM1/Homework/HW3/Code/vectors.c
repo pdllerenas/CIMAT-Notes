07da7b9841fb34147e804bd1dcdf4f2526801b62
pdllerenas <pdllerenas@gmail.com>
2025-09-13 17:20:29 -0600

    NUM1 HW3: vector reader accepts filename instead of opened file pointer
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/NUM1/Homework/HW3/Code/vectors.c
+++ Masters/Semester1/NUM1/Homework/HW3/Code/vectors.c
@@ -1,6 +1,12 @@
-#include <stdlib.h>
 #include "vectors.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/*
+
+reads the file and modifies dim to the number of new lines found.
+
+ */
 void get_vector_dim(FILE *file, int *dim) {
   int c;
   *dim = 0;
@@ -12,6 +18,13 @@ void get_vector_dim(FILE *file, int *dim) {
   }
   rewind(file);
 }
+
+/*
+
+reads the matrix file and edits the cols and rows parameters passed. We assume
+space as the separator, \n as the newline.
+
+ */
 
 void get_matrix_dims(FILE *file, int *cols, int *rows) {
   int c;
@@ -28,7 +41,19 @@ void get_matrix_dims(FILE *file, int *cols, int *rows) {
   rewind(file);
 }
 
-double *file_to_vector(FILE *file, int *dim) {
+/*
+
+reads and parses the file, returning a pointer. We assume the endline char is
+\n.
+
+ */
+
+double *file_to_vector(const char *filename, int *dim) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    perror(filename);
+    return NULL;
+  }
   *dim = 0;
   get_vector_dim(file, dim);
 
@@ -37,18 +62,27 @@ double *file_to_vector(FILE *file, int *dim) {
   if (vec == NULL) {
     fprintf(stderr, "Could not allocate memory for vector.\n");
     fclose(file);
-    exit(1);
+    return NULL;
   }
   int row_index = 0;
   while (fgets(line, sizeof line, file) && row_index < *dim) {
     vec[row_index] = atof(line);
     row_index++;
   }
+  fclose(file);
   return vec;
 }
 
+/*
+
+dot product of x and y. We iterate only dim positions for both x and y. We also
+assume x and y have enough memory allocated for it to iterate through them.
+
+ */
+
 double dot(double *x, double *y, int dim) {
   double sum = 0;
+
   for (int i = 0; i < dim; i++) {
     sum += *(x + i) * *(y + i);
   }
--- Masters/Semester1/NUM1/Homework/HW3/Code/vectors.h
+++ Masters/Semester1/NUM1/Homework/HW3/Code/vectors.h
@@ -2,5 +2,5 @@
 
 void get_vector_dim(FILE *file, int *dim);
 void get_matrix_dims(FILE *file, int *cols, int *rows);
-double *file_to_vector(FILE *file, int *dim);
+double *file_to_vector(const char* filename, int *dim);
 double dot(double *x, double *y, int dim);
