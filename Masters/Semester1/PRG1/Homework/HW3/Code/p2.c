a45483e813cd66866c6a7acf00bf30a70e4cb7ff
pdllerenas <pdllerenas@gmail.com>
2025-09-18 22:52:44 -0600

    PRG1 HW3: adjusted function calls in p2.c to new header file
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/PRG1/Homework/HW3/Code/p2.c
+++ Masters/Semester1/PRG1/Homework/HW3/Code/p2.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "convolution.h"
 #include "kernel_io.h"
 #include "pgm_io.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
   if (argc != 4) {
-    fprintf(stderr, "Invalid argument count.\nUsage: %s <input-file-path> <output-file-path> <filter>\n\nAvailable filters:\n'blur3', 'blur5', 'blur7'.\n", argv[0]);
+    fprintf(stderr,
+            "Invalid argument count.\nUsage: %s <input-file-path> "
+            "<output-file-path> <filter>\n\nAvailable filters:\n'scharr', "
+            "'gradient', 'laplacian4', 'laplacian20'.\n",
+            argv[0]);
     exit(1);
   }
   int i, j, max, ok, rows, cols;
@@ -26,31 +27,37 @@ int main(int argc, char *argv[])
 
   // Procesamiento de la imagen: ** INCLUIR SU CODIGO AQUI **
   unsigned char **image_out;
-  Kernel *(*kfp)();
-  Kernel *(*hor)() = create_horizontal_schar;
-  Kernel *(*ver)() = create_vertical_schar;
-  Kernel *(*l4)() = create_laplacian_4;
-  Kernel *(*l20)() = create_laplacian_20;
-  // if (strcmp(argv[3], "blur3") == 0) {
-  //   kfp = create_mean_3x3;
-  // } else if (strcmp(argv[3], "blur5") == 0) {
-  //   kfp = create_mean_5x5;
-  // } else if (strcmp(argv[3], "blur7") == 0) {
-  //   kfp = create_mean_7x7;
-  // }
-  // else {
-  //   fprintf(stderr, "Invalid filter: %s\n", argv[3]);
-  //   exit(1);
-  // }
-  print_kernel(hor());
+  Kernel *(*kfp1)() = NULL;
+  Kernel *(*kfp2)() = NULL;
 
-  print_kernel(ver());
-  
-  // image_out = gradient_filter(image, rows, cols);
-  image_out = apply_sobel(image, rows, cols, l20, l20);
+  if (strcmp(argv[3], "scharr") == 0) {
+    kfp1 = create_vertical_schar;
+    kfp2 = create_horizontal_schar;
+    if (kfp1 != NULL && kfp2 != NULL) {
+      image_out = apply_sobel(image, rows, cols, kfp1, kfp2);
+    }
+  } else if (strcmp(argv[3], "gradient") == 0) {
+    image_out = apply_gradient(image, rows, cols);
+  } else if (strcmp(argv[3], "laplacian4") == 0) {
+    kfp1 = create_laplacian_4;
+    if (kfp1 != NULL) {
+      image_out = apply_gaussian(image, rows, cols, kfp1);
+    }
+  } else if (strcmp(argv[3], "laplacian20") == 0) {
+    kfp1 = create_laplacian_20;
+    if (kfp1 != NULL) {
+      image_out = apply_gaussian(image, rows, cols, kfp1);
+    }
+  } else {
+    fprintf(stderr, "Invalid filter: %s\n", argv[3]);
+    exit(1);
+  }
 
   // Archivo de salida
   ok = pgmWrite(argv[2], rows, cols, image_out, NULL);
+  free(image[0]);
   free(image);
+
+  free(image_out[0]);
   free(image_out);
 }
