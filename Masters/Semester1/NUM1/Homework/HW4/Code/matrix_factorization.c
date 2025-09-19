fbc95b0113a31616f852bee94a753b9dc4237f03
pdllerenas <pdllerenas@gmail.com>
2025-09-19 08:10:44 -0600

    NUM1 HW4: added doolittle and cholesky decompositions
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/NUM1/Homework/HW4/Code/matrix_factorization.c
+++ Masters/Semester1/NUM1/Homework/HW4/Code/matrix_factorization.c
@@ -1,8 +1,56 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "matrix_factorization.h"
 
+void cholesky(double const **A, double **L, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      double sum = 0;
+      for (int k = 0; k < j; k++) {
+        sum += L[i][k] * L[j][k];
+      }
+
+      if (i == j) {
+        L[i][j] = sqrt(A[i][j] - sum);
+      } else {
+        L[i][j] = (1.0 / L[i][j]*(A[i][j] - sum));
+      }
+    }
+  }
+}
+
+void doolittle(double const **A, double **L, double **U, int n) {
+	int i, j, k;
+	double sum = 0;
+
+	for (i = 0; i < n; i++) {
+		L[i][i] = 1;
+	}
+
+	for (j = 0; j < n; j++) {
+		for (i = j; i < n; i++) {
+			sum = 0;
+			for (k = 0; k < j; k++) {
+				sum += U[i][k] * L[k][j];	
+			}
+			U[i][j] = A[i][j] - sum;
+		}
+
+		for (i = j; i < n; i++) {
+			sum = 0;
+			for(k = 0; k < j; k++) {
+				sum = sum + U[j][k] * L[k][i];
+			}
+			if (U[j][j] == 0) {
+				fprintf(stderr, "Can't divide by 0.\n");
+				exit(EXIT_FAILURE);
+			}
+			L[j][i] = (A[j][i] - sum) / U[j][j];
+		}
+	}
+}
 
 void crout(double const **A, double **L, double **U, int n) {
 	int i, j, k;
@@ -27,7 +75,7 @@ void crout(double const **A, double **L, double **U, int n) {
 				sum = sum + L[j][k] * U[k][i];
 			}
 			if (L[j][j] == 0) {
-				printf("det(L) close to 0!\n Can't divide by 0...\n");
+				fprintf(stderr, "Can't divide by 0.\n");
 				exit(EXIT_FAILURE);
 			}
 			U[j][i] = (A[j][i] - sum) / L[j][j];
--- Masters/Semester1/NUM1/Homework/HW4/Code/matrix_factorization.h
+++ Masters/Semester1/NUM1/Homework/HW4/Code/matrix_factorization.h
@@ -1,1 +1,4 @@
 void crout(double const **A, double **L, double **U, int n);
+void doolittle(double const **A, double **L, double **U, int n);
+void cholesky(double const **A, double **L, int n);
+
