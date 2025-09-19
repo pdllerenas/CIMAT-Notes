a88644b5518a8bf2c2253b9d355a8870ec0e620e
pdllerenas <pdllerenas@gmail.com>
2025-09-18 22:52:32 -0600

    PRG1 HW3: adjusted function calls to new header file
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/PRG1/Homework/HW3/Code/p1.c
+++ Masters/Semester1/PRG1/Homework/HW3/Code/p1.c
@@ -6,11 +6,16 @@
 #include "kernel_io.h"
 #include "pgm_io.h"
 
-int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    fprintf(stderr, "Invalid argument count.\nUsage: %s <input-file-path> <output-file-path> <filter>\n\nAvailable filters:\n'blur3', 'blur5', 'blur7'.\n", argv[0]);
+int main(int argc, char *argv[]) {
+  if (argc != 5) {
+    fprintf(stderr,
+            "Invalid argument count.\nUsage: %s <input-file-path> "
+            "<output-file-path> <filter> <window-size>\n\nAvailable "
+            "filters:\n'mean', 'median', 'gaussian',\n ",
+            argv[0]);
     exit(1);
   }
+  int k_dim = atoi(argv[4]); // windows size for kernel
   int i, j, max, ok, rows, cols;
   int count = 0;
   unsigned char **image;
@@ -24,24 +29,40 @@ int main(int argc, char* argv[]) {
 
   // Procesamiento de la imagen: ** INCLUIR SU CODIGO AQUI **
   unsigned char **image_out;
-  Kernel *(*kfp)();
-  if (strcmp(argv[3], "blur3") == 0) {
-    kfp = create_mean_3x3;
-  } else if (strcmp(argv[3], "blur5") == 0) {
-    kfp = create_mean_5x5;
-  } else if (strcmp(argv[3], "blur7") == 0) {
-    kfp = create_mean_7x7;
-  }
-  else {
+  Kernel *(*gaussian_kernel)() = NULL; // fn pointer for kernel creation
+  Kernel *(*mean_kernel)(int) = NULL; // fn pointer for nxn mean kernel creation
+
+  // set fn pointer to appropriate one, considering k_dim
+  if (strcmp(argv[3], "gaussian") == 0) {
+    if (k_dim == 3) {
+      gaussian_kernel = create_gaussian_3x3;
+    } else if (k_dim == 5) {
+      gaussian_kernel = create_gaussian_5x5;
+    } else if (k_dim == 7) {
+      gaussian_kernel = create_gaussian_7x7;
+    } else {
+      fprintf(stderr, "Invalid gaussian kernel dimension. Try: 3, 5, or 7.\n");
+      exit(1);
+    }
+  } else if (strcmp(argv[3], "mean") == 0) {
+    mean_kernel = create_mean_nxn;
+  } else if (strcmp(argv[3], "median") == 0) {
+    // median filter has no kernel, so we directly edit the image file
+    image_out = median_filter_nxn(image, rows, cols, k_dim);
+  } else {
     fprintf(stderr, "Invalid filter: %s\n", argv[3]);
     exit(1);
   }
-  print_kernel(kfp());
-  
-  image_out = apply_filter(image, rows, cols, kfp);
+  if (gaussian_kernel != NULL) {
+    image_out = apply_gaussian(image, rows, cols, gaussian_kernel);
+  } else if (mean_kernel != NULL) {
+    image_out = apply_mean(image, rows, cols, mean_kernel, k_dim);
+  }
 
   // Archivo de salida
   ok = pgmWrite(argv[2], rows, cols, image_out, NULL);
+  free(image[0]);
   free(image);
+  free(image_out[0]);
   free(image_out);
 }
