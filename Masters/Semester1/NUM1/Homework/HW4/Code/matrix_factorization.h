37ee0b799f97e954125f87b197f6bb6f1e4b39f5
pdllerenas <pdllerenas@gmail.com>
2025-09-18 22:57:58 -0600

    NUM1 HW4: boilerplating for homework
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

---
+++ Masters/Semester1/NUM1/Homework/HW4/Code/matrix_factorization.h
@@ -0,0 +1,1 @@
+void crout(double const **A, double **L, double **U, int n);
---
+++ Masters/Semester1/NUM1/Homework/HW4/Code/p1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void crout(double const **A, double **L, double **U, int n) {
+	int i, j, k;
+	double sum = 0;
+
+	for (i = 0; i < n; i++) {
+		U[i][i] = 1;
+	}
+
+	for (j = 0; j < n; j++) {
+		for (i = j; i < n; i++) {
+			sum = 0;
+			for (k = 0; k < j; k++) {
+				sum = sum + L[i][k] * U[k][j];	
+			}
+			L[i][j] = A[i][j] - sum;
+		}
+
+		for (i = j; i < n; i++) {
+			sum = 0;
+			for(k = 0; k < j; k++) {
+				sum = sum + L[j][k] * U[k][i];
+			}
+			if (L[j][j] == 0) {
+				printf("det(L) close to 0!\n Can't divide by 0...\n");
+				exit(EXIT_FAILURE);
+			}
+			U[j][i] = (A[j][i] - sum) / L[j][j];
+		}
+	}
+}
