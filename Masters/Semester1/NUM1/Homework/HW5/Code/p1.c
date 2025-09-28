5e8445714c054ca158e0ccb409732154db57d5bc
pdllerenas <pdllerenas@gmail.com>
2025-09-28 14:22:29 -0600

    NUM1 HW5: code for big eigenvalue matrix
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/NUM1/Homework/HW5/Code/p1.c
+++ Masters/Semester1/NUM1/Homework/HW5/Code/p1.c
@@ -1,16 +1,14 @@
 #include "../../utils/linalg/eigenvalues.h"
-#include "../../utils/linalg/linear_system.h"
 #include "../../utils/linalg/matrix.h"
-#include "../../utils/linalg/matrix_factorization.h"
 #include "../../utils/linalg/matrix_io.h"
 #include "../../utils/linalg/vector.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
+  if (argc != 6) {
     fprintf(stderr,
-            "Invalid argument count. \nUsage: %s <matrix-input> <vector-input>",
+            "Invalid argument count. \nUsage: %s <matrix-input> <vector-input> <m-values> <TOLERANCE> <MAX_ITER>",
             argv[0]);
     exit(1);
   }
@@ -26,10 +24,28 @@ int main(int argc, char *argv[]) {
     matrix_free(A);
     exit(1);
   }
-  double lambda = iterative_power(A, b, 1e-6, 100);
-  // Using A = LU, we have LUx = b, so we solve Ly = b and then Ux = y.
-  printf("%lf\n", lambda);
+
+  int m = atoi(argv[3]);
+  double TOL = strtod(argv[4], NULL);
+  int MAX_ITER = atoi(argv[5]);
+  double *eigenvalues = malloc(m * sizeof(double));
+  Vector **eigenvector = malloc(m * sizeof(Vector *));
+
+  get_m_largest_eigenvalues(A, b, m, TOL, MAX_ITER, eigenvalues, eigenvector);
+  for (int i = 0; i < m; i++) {
+    printf("Eigenvector: ");
+    print_vector(eigenvector[i]);
+
+    printf("Eigenvalue: ");
+    printf("%lf\n\n", eigenvalues[i]);
+  }
+
   matrix_free(A);
   free_vector(b);
+  free(eigenvalues);
+  for (int i = 0; i < m; i++) {
+    free_vector(eigenvector[i]);
+  }
+  free(eigenvector);
   return 0;
 }
