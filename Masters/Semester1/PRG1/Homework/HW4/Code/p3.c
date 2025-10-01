d49e30d63b4baf326ac9e781c00b9379f17f01b3
pdllerenas <pdllerenas@gmail.com>
2025-10-01 08:44:06 -0600

    PRG1 HW4: cleaned up code and documented
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/PRG1/Homework/HW4/Code/p3.c
+++ Masters/Semester1/PRG1/Homework/HW4/Code/p3.c
@@ -3,11 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+
+calculates the frequency of each pixel, assuming there are only 256 possible
+values for the given image (which is true for pgm file)
+
+ */
 unsigned int *image_freq(unsigned char **image, int rows, int cols) {
-  unsigned int n_bytes = 0;
-  unsigned int n_letters = 0;
-  char c, previous_char = 0;
   unsigned int *frequency = calloc(256, sizeof(unsigned int));
+  if (!frequency) {
+    perror("Could not allocate memory.\n");
+    exit(1);
+  }
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
       frequency[image[i][j]]++;
@@ -16,6 +23,12 @@ unsigned int *image_freq(unsigned char **image, int rows, int cols) {
   return frequency;
 }
 
+/*
+
+calculates the shannon entropy of an image, assuming 8 bits per pixel.
+space_size refers to the total number of pixels in the image
+
+ */
 double shannon_entropy(unsigned int *frequencies, int space_size) {
   double sum = 0.0;
   for (int i = 0; i < 256; i++) {
@@ -32,8 +45,14 @@ double shannon_entropy(unsigned int *frequencies, int space_size) {
 int main(int argc, char *argv[]) {
   int rows, cols;
   unsigned char **image = pgmRead(argv[1], &rows, &cols);
+  if (image == NULL) {
+    printf("Error pgmRead\n");
+    exit(1);
+  }
   unsigned int *freq = image_freq(image, rows, cols);
-  double sum = 0.0;
+  if (!freq) {
+    exit(1);
+  }
   printf("entropy = %lf\n", shannon_entropy(freq, rows * cols));
 
   free(freq);
