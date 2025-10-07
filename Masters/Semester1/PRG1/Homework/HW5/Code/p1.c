8c902f3672ff61019af80a397b84f1afc00d9a54
pdllerenas <pdllerenas@gmail.com>
2025-10-06 18:53:51 -0600

    PRG1 HW5: initial skeleton, todo: apply algorithm to min-heap of nodes
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

---
+++ Masters/Semester1/PRG1/Homework/HW5/Code/p1.c
@@ -0,0 +1,16 @@
+#include "huffman_encoder.h"
+#include "pgm_io.h"
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Invalid argument count. Usage: %s <file-name>\n", argv[0]);
+    exit(1);
+  }
+  int *frequencies;
+  get_freq_table(argv[1], &frequencies);
+
+  PriorityQueue *pq = enqueue_alphabet(frequencies, 256);
+}
