050ad29ef834b343b4b8d01188b4257e83eda1cc
pdllerenas <pdllerenas@gmail.com>
2025-10-06 18:47:59 -0600

    NUM1 HW6: solution to system of equaitons using conjugate gradient
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/NUM1/Homework/HW6/Code/p5.c
+++ Masters/Semester1/NUM1/Homework/HW6/Code/p5.c
@@ -2,20 +2,19 @@
 #include "../../utils/linalg/eigenvalues.h"
 #include "../../utils/linalg/matrix_io.h"
 #include "../../utils/linalg/matrix.h"
+#include "../../utils/linalg/vector.h"
 
 
 
 int main(int argc, char *argv[])
 {
-
   int rows, cols;
   
   Matrix *A = load_matrix_from_txt(argv[1], &rows, &cols);
   Vector *b = load_vector_from_txt(argv[2]);
   Vector *x0 = load_vector_from_txt(argv[3]);
   
-  double s = matrix_conjugate_with_vector(A, x0);
-
-  Vector *x = conjugate_gradient(A, b, x0, 1e-6, 100);
-  print_tex_table(x);
+  Vector *x = conjugate_gradient(A, b, x0, 1e-12, 100);
+  print_vector(x);
+  
 }
