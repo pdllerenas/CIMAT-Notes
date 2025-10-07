1286df9f450ee1fd973ecd0bf0d618919909eb62
pdllerenas <pdllerenas@gmail.com>
2025-10-07 08:02:44 -0600

    added huffman tree creation and printer
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/PRG1/Homework/HW5/Code/huffman_encoder.c
+++ Masters/Semester1/PRG1/Homework/HW5/Code/huffman_encoder.c
@@ -19,13 +19,32 @@ PriorityQueue *enqueue_alphabet(int *frequencies, int size) {
     }
     pq_enqueue(pq, hn);
   }
-  pq_print_queue(pq);
   return pq;
 }
 
-void generate_code_table(PriorityQueue *heap) {
+HuffmanNode *huffman_tree_parent_node(PriorityQueue *heap) {
   while (heap->size > 1) {
-    dequeue(heap);
-
+    HuffmanNode *h1 = dequeue(heap);
+    HuffmanNode *h2 = dequeue(heap);
+    HuffmanNode *hp = hn_create_node(' ', h1->freq + h2->freq);
+    hp->left = h1;
+    hp->right = h2;
+    pq_enqueue(heap, hp);
   }
+  return heap->items[0];
+}
+
+void print_huffman_tree(HuffmanNode *root, int depth) {
+  if (!root)
+    return;
+  print_huffman_tree(root->right, depth + 1);
+  for (int i = 0; i < depth; i++) {
+    printf("    ");
+  }
+  if (root->right || root->left) {
+    printf("+\n");
+  } else {
+    printf("%d\n", root->freq);
+  }
+  print_huffman_tree(root->left, depth + 1);
 }
--- Masters/Semester1/PRG1/Homework/HW5/Code/huffman_encoder.h
+++ Masters/Semester1/PRG1/Homework/HW5/Code/huffman_encoder.h
@@ -3,3 +3,5 @@
 #include "queue.h"
 
 PriorityQueue *enqueue_alphabet(int *frequencies, int size);
+HuffmanNode *huffman_tree_parent_node(PriorityQueue *heap);
+void print_huffman_tree(HuffmanNode *root, int depth);
