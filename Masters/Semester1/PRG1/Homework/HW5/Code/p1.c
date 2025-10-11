8341b228ec0e3cae5781eab4ced2559914578395
pdllerenas <pdllerenas@gmail.com>
2025-10-10 22:45:54 -0600

    testing writing to disk with lena encryption
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/PRG1/Homework/HW5/Code/p1.c
+++ Masters/Semester1/PRG1/Homework/HW5/Code/p1.c
@@ -1,8 +1,21 @@
 #include "huffman_encoder.h"
 #include "pgm_io.h"
 #include "queue.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void printbits(uint64_t n, int length) {
+  uint64_t i;
+  i = 1UL << length;
+  while (i > 0) {
+    if (n & i)
+      printf("1");
+    else
+      printf("0");
+    i >>= 1;
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -14,6 +27,29 @@ int main(int argc, char *argv[]) {
 
   PriorityQueue *pq = enqueue_alphabet(frequencies, 256);
   HuffmanNode *huff_parent = huffman_tree_parent_node(pq);
-  print_huffman_tree(huff_parent, 0);
+  HuffmanCode **table = malloc(256 * sizeof(HuffmanCode *));
+  for (int i = 0; i < 256; i++) {
+    table[i] = innit_huffman_code();
+  }
 
+  int leafs = huffman_code_table(huff_parent, 0, 0, table);
+  for (int i = 0; i < 256; i++) {
+    if (table[i]->length) {
+      printf("%d --- ", i);
+      printbits(table[i]->code, table[i]->length);
+      printf("\n");
+    }
+  }
+
+  int rows, cols;
+  unsigned char **image = pgmRead(argv[1], &rows, &cols);
+
+  FILE *fp = fopen("lena_huff.bin", "wb");
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      unsigned char c = image[i][j];
+			fwrite(fp);
+    }
+  }
 }
