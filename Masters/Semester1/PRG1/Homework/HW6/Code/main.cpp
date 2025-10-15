481ee7ecaf220a700fce0ee03629fdc58c9b5550
pdllerenas <pdllerenas@gmail.com>
2025-10-15 17:50:48 -0600

    PRG1 HW6: execution time diff leap years
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- Masters/Semester1/PRG1/Homework/HW6/Code/main.cpp
+++ Masters/Semester1/PRG1/Homework/HW6/Code/main.cpp
@@ -1,38 +1,41 @@
+/*
+ * TAREA 6 PRG1
+ * PEDRO DAVID LLERENAS GONZÁLEZ
+ */
+
+
+
 #include <chrono>
 #include <iostream>
 
-// https://en.cppreference.com/w/cpp/chrono/high_resolution_clock/now.html
 volatile bool sink; // prevents optimization
 
-void loop_func(int start, int end, bool (*f)(unsigned int)) {
-  for (int i = start; i <= end; i++) {
-    sink = f(i);
-  }
-}
-
 bool gpt_leap_year(unsigned int year) {
-  if (year % 4 != 0) {
-    return false;
-  }
-  if (year % 100 != 0) {
-    return true;
-  }
   if (year % 400 == 0) {
+    return true;
+  } else if (year % 100 == 0) {
+    return false;
+  } else if (year % 4 == 0) {
     return true;
   }
   return false;
 }
 
 bool microsoft_v1_leap_year(unsigned int year) {
-  if (year % 4 != 0)
+  if (year % 4 != 0) {
     return false;
-  return (year % 100 != 0 || year % 400 == 0);
+  }
+  if (year % 400 == 0) {
+    return true;
+  }
+  if (year % 100 == 0) {
+    return false;
+  }
+  return true;
 }
 
 bool microsoft_v2_leap_year(unsigned int year) {
-  if (year % 4 != 0)
-    return false;
-  return (year % 100 != 0 || year % 400 == 0);
+  return ((year % 4) == 0) && ((year % 100) != 0 || (year % 400) == 0);
 }
 
 bool optimized_400_leap_year(unsigned int year) {
@@ -52,52 +55,58 @@ bool optimized_16_leap_year(unsigned int year) {
 }
 
 bool proposed_leap_year(unsigned int year) {
-  return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+  return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+}
+
+// wrapper function to loop function calls of f,
+// we use the volatile variable here to "prevent optimization",
+// as indicated by
+// https://en.cppreference.com/w/cpp/chrono/steady_clock/now.html
+void loop_func(int start, int end, bool (*f)(unsigned int)) {
+  for (int i = start; i <= end; i++) {
+    sink = f(i);
+  }
+}
+
+// prints the average execution time of the function f, over iter iterations
+void benchmark(int iter, int index_start, int index_end,
+               bool (*f)(unsigned int), std::string fn_name) {
+  std::chrono::duration<double> avg;
+  for (int i = 0; i < iter; i++) {
+    auto start = std::chrono::steady_clock::now();
+    loop_func(index_start, index_end, f);
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    avg += diff;
+  }
+  avg /= iter;
+  // std::fixed prevents cout from printing avg.count in scientific notation
+  std::cout << std::fixed << fn_name << ": " << avg.count() * 1000 << " ms"
+            << std::endl;
 }
 
 int main(int argc, char *argv[]) {
-	int iter_s = 0, iter_e = 200000;
+  if (argc != 4) {
+    fprintf(
+        stderr,
+        "Invalid argument count: Usage: %s <trials> <year-start> <year-end>\n",
+        argv[0]);
+    exit(1);
+  }
+  int iter_s = atoi(argv[2]), iter_e = atoi(argv[3]);
+  int trials = atoi(argv[1]);
 
   printf("===========================\n");
-  auto start = std::chrono::high_resolution_clock::now();
-  loop_func(iter_s, iter_e, gpt_leap_year);
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> diff = end - start;
-  std::cout << "gpt_leap_year: " << diff.count() << std::endl;
-
+  benchmark(trials, iter_s, iter_e, gpt_leap_year, "gpt");
   printf("===========================\n");
-  start = std::chrono::high_resolution_clock::now();
-  loop_func(iter_s, iter_e, microsoft_v1_leap_year);
-  end = std::chrono::high_resolution_clock::now();
-  diff = end - start;
-  std::cout << "microsoft v1: " << diff.count() << std::endl;
-
+  benchmark(trials, iter_s, iter_e, microsoft_v1_leap_year, "ms v1");
   printf("===========================\n");
-  start = std::chrono::high_resolution_clock::now();
-  loop_func(iter_s, iter_e, microsoft_v2_leap_year);
-  end = std::chrono::high_resolution_clock::now();
-  diff = end - start;
-  std::cout << "microsoft v2: " << diff.count() << std::endl;
-
+  benchmark(trials, iter_s, iter_e, microsoft_v2_leap_year, "ms v2");
   printf("===========================\n");
-  start = std::chrono::high_resolution_clock::now();
-  loop_func(iter_s, iter_e, optimized_400_leap_year);
-  end = std::chrono::high_resolution_clock::now();
-  diff = end - start;
-  std::cout << "optimized 400: " << diff.count() << std::endl;
-
+  benchmark(trials, iter_s, iter_e, optimized_400_leap_year, "optimized 400");
   printf("===========================\n");
-  start = std::chrono::high_resolution_clock::now();
-  loop_func(iter_s, iter_e, optimized_16_leap_year);
-  end = std::chrono::high_resolution_clock::now();
-  diff = end - start;
-  std::cout << "optimized 16: " << diff.count() << std::endl;
-
+  benchmark(trials, iter_s, iter_e, optimized_16_leap_year, "optimized 16");
   printf("===========================\n");
-  start = std::chrono::high_resolution_clock::now();
-  loop_func(iter_s, iter_e, proposed_leap_year);
-  end = std::chrono::high_resolution_clock::now();
-  diff = end - start;
-  std::cout << "proposed: " << diff.count() << std::endl;
+  benchmark(trials, iter_s, iter_e, proposed_leap_year, "proposed");
   printf("===========================\n");
 }
